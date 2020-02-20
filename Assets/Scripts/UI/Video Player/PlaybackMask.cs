b8b389249a6bae7fa8395752de2c4b4cfa920f61
John Asuncion <johnasuncion0304@gmail.com>
2020-02-20 17:21:45 +0800

    Animation for playback options

---
+++ Assets/Scripts/UI/Video Player/PlaybackMask.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlaybackMask : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public Animator animator;
+
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        animator.SetTrigger("Up");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        animator.SetTrigger("Down");
+    }
+}
