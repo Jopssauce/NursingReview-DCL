5b308438bc884b08fdb82d74193ca063d4cbc3ea
John Asuncion <johnasuncion0304@gmail.com>
2020-02-21 11:07:46 +0800

    Scrolling Gridlines

---
+++ Assets/Scripts/ScrollBackground.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollBackground : MonoBehaviour
+{
+    public float speed = 1f;
+    public float offset;
+
+    RectTransform rectTransform;
+    Vector2 startPos;
+    float newPos;
+
+    private void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        startPos = rectTransform.position;
+    }
+
+    private void Update()
+    {
+        newPos = Mathf.Repeat(Time.time * speed, offset);
+        rectTransform.position = startPos + Vector2.right * newPos;
+    }
+
+}
