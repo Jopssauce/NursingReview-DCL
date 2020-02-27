76e8b33cc7d68817c27d904c04c59c33149669a2
John Asuncion <johnasuncion0304@gmail.com>
2020-02-27 15:41:37 +0800

    Added scrolling and zooming of open card

---
+++ Assets/Scripts/UI/UIScrollImage.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class UIScrollImage : MonoBehaviour, IScrollHandler
+{
+    public float ZoomSpeed = 0.1f;
+    public float MaxZoom = 10f;
+
+    Vector3 initialScale;
+
+    private void Awake()
+    {
+        initialScale = transform.localScale;
+    }
+
+    public void OnScroll(PointerEventData eventData)
+    {
+
+        Vector3 delta = Vector3.one * (eventData.scrollDelta.y * ZoomSpeed);
+        Vector3 desiredScale = transform.localScale + delta;
+
+        desiredScale = ClampedDesiredScale(desiredScale);
+        transform.localScale = desiredScale;
+    }
+
+    Vector3 ClampedDesiredScale(Vector3 scale)
+    {
+        scale = Vector3.Max(initialScale, scale);
+        scale = Vector3.Min(initialScale * MaxZoom, scale);
+        return scale;
+    }
+}
