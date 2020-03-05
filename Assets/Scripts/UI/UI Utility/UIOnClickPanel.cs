0ddae2c7c766469c7873e143ad526adbb4161527
John Asuncion <johnasuncion0304@gmail.com>
2020-03-05 15:47:27 +0800

    On Click Panel Prefab

---
+++ Assets/Scripts/UI/UI Utility/UIOnClickPanel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class UIOnClickPanel : MonoBehaviour, IPointerClickHandler
+{
+    public Image Panel;
+
+    public UnityEvent onClick;
+
+    private void OnEnable()
+    {
+        Panel.GetComponent<RectTransform>().sizeDelta = new Vector2(Screen.width, Screen.height);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        onClick.Invoke();
+    }
+}
