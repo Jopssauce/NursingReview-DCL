6bc66ca4f114ccb34b1df12e36d694bcc48fd9b5
John Asuncion <johnasuncion0304@gmail.com>
2020-03-05 21:41:04 +0800

    Title Screen

---
+++ Assets/Scripts/UI/UI Controllers/UITitleScreen.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using DG.Tweening;
+
+public class UITitleScreen : UIController
+{
+    public CanvasGroup canvasGroup;
+    public override void LoadActiveScene(string sceneToLoad)
+    {
+        Tween tween = canvasGroup.DOFade(0, 0.3f);
+        tween.onComplete += delegate ()
+        {
+            base.LoadActiveScene(sceneToLoad);
+        };
+    }
+}
