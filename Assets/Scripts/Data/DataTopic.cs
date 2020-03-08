a7f4fdc693a2ea2e0db5c9b6286480be03055823
John Asuncion <johnasuncion0304@gmail.com>
2020-03-08 15:18:06 +0800

    Video url

--- Assets/Scripts/Data/DataTopic.cs
+++ Assets/Scripts/Data/DataTopic.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class DataTopic : ScriptableObject
 {
     public string TopicName;
+    public string VideoUrl;
     public Sprite Background;
     public List<DataSubTopic> SubTopics;
 }
