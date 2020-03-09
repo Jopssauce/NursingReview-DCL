a9732d18118d665111bcac49aded1d5c9a17cca7
John Asuncion <johnasuncion0304@gmail.com>
2020-03-09 12:18:58 +0800

    Double click to zoom

--- Assets/Scripts/UI/Card Behaviour/CardFace.cs
+++ Assets/Scripts/UI/Card Behaviour/CardFace.cs
@@ -12,20 +12,89 @@ public class CardFace : MonoBehaviour, IPointerClickHandler, IBeginDragHandler, 
     public Image BackFace;
     public ScrollRect scrollRect;
 
+    GameObject currentFace;
+
     bool isBack = true;
     bool isDrag;
+    bool isZoomed;
+
+    Vector3 initialScale;
+
+    //properties
+    public float MaxTimeToClick = 0.60f;
+    public float MinTimeToClick = 0.05f;
+    public bool IsDebug = false;
+    public bool isDoubleClicked;
+
+    //private variables to keep track
+    private float _minCurrentTime;
+    private float _maxCurrentTime;
+
+    Coroutine clickRoutine;
+
+    public bool DoubleClick()
+    {
+        if (Time.time >= _minCurrentTime && Time.time <= _maxCurrentTime)
+        {
+            if (IsDebug)
+                Debug.Log("Double Click");
+            _minCurrentTime = 0;
+            _maxCurrentTime = 0;
+            isDoubleClicked = true;
+            return true;
+        }
+        _minCurrentTime = Time.time + MinTimeToClick; _maxCurrentTime = Time.time + MaxTimeToClick;
+        isDoubleClicked = false;
+        return false;
+    }
+
+    IEnumerator DetectClick()
+    {
+        yield return new WaitForSeconds(0.2f);
+        if (isDoubleClicked == false)
+        {
+            SwitchFaces();
+        }
+        else
+        {
+            if (isZoomed)
+            {
+                UnZoomFace(currentFace);
+            }
+            else
+            {
+                ZoomFace(currentFace);
+            }
+            StopCoroutine(clickRoutine);
+            yield break;
+        }
+    }
 
     void OnEnable()
     {
         FrontFace.sprite = CardData.FrontFace;
         BackFace.sprite = CardData.BackFace;
         FrontFace.transform.localScale = Vector3.one;
+        currentFace = FrontFace.gameObject;
+        initialScale = currentFace.transform.localScale;
         //FrontFace.transform.position += new Vector3(0, 200, 0);
     }
 
     public void OnPointerClick(PointerEventData eventData)
     {
-        if(isDrag == false) SwitchFaces();
+        DoubleClick();
+        if (clickRoutine != null)
+        {
+            StopCoroutine(clickRoutine);
+            clickRoutine = null;
+        }
+        if(isDrag == false)
+        {
+            if(clickRoutine == null)
+            {
+                clickRoutine = StartCoroutine(DetectClick());
+            }
+        }
     }
 
     private void SwitchFaces()
@@ -43,6 +112,7 @@ public class CardFace : MonoBehaviour, IPointerClickHandler, IBeginDragHandler, 
     //Switches faceA with faceB
     private void SwitchFace(GameObject faceA, GameObject faceB)
     {
+        currentFace = faceA.gameObject;
         faceA.gameObject.SetActive(true);
 
         Tween tween = faceA.transform.DORotate(new Vector3(0, 0, 0), 0.3f);
@@ -55,6 +125,18 @@ public class CardFace : MonoBehaviour, IPointerClickHandler, IBeginDragHandler, 
             faceB.gameObject.SetActive(false);
 
         isBack = !isBack;
+    }
+
+    public void ZoomFace(GameObject current)
+    {
+        isZoomed = true;
+        current.transform.DOScale(2, 0.3f);
+    }
+    
+    public void UnZoomFace(GameObject current)
+    {
+        isZoomed = false;
+        current.transform.DOScale(1, 0.3f);
     }
 
     public void OnBeginDrag(PointerEventData eventData)
